6e7d385c59bc556e15969b9041ec0e1fb1cc77a0
Emilse <emilsearias@gmail.com>
2021-11-11 11:42:10 -0300

    agregamos la funcion consultarAPI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Frase from './components/Frase';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(()=>{
+    consultarAPI();
+  },[])
+
+  const consultarAPI = async() =>{
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const dato = await respuesta.json();
+    console.log(respuesta)
+    console.log(dato[0]);
+    // guardar en el state
+    setPersonaje(dato[0]);
+  }
+
   return (
     <section className='container my-5 d-flex flex-column align-items-center'>
       <img src={logo} alt="logo de los simpsons" />
