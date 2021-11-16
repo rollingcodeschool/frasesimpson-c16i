e0227c0512701729d61aa13fb3b303c8df57980b
Emilse <emilsearias@gmail.com>
2021-11-16 11:36:01 -0300

    se agregaron ejemplos de codigo del operador ternario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.png';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Frase from './components/Frase';
-import Spinner from './components/Spinner';
-import {useState, useEffect} from 'react';
+import logo from "./logo.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
+import { useState, useEffect } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
-  const [carga, setCarga ] = useState(false);
+  const [carga, setCarga] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     consultarAPI();
-  },[])
+  }, []);
 
-  const consultarAPI = async() =>{
+  const consultarAPI = async () => {
     setCarga(false);
-    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
     const dato = await respuesta.json();
-    console.log(respuesta)
+    console.log(respuesta);
     console.log(dato[0]);
     setTimeout(() => {
       // guardar en el state
@@ -26,16 +28,24 @@ function App() {
       // mostrar el componente frase
       setCarga(true);
     }, 2500);
-  }
+  };
   // operador ternario
   // (condicion logica)?(codigo a ejecutar cuando es verdadera):(codigo cuando la condicion es false)
-  const mostrarComponente = (carga === true)?(<Frase personaje={personaje}></Frase>):(      <Spinner/>)
+  const mostrarComponente =
+    carga === true ? <Frase personaje={personaje}></Frase> : <Spinner />;
 
   return (
-    <section className='container my-5 d-flex flex-column align-items-center'>
+    <section className="container my-5 d-flex flex-column align-items-center">
       <img src={logo} alt="logo de los simpsons" />
-      <Button variant='warning' className='my-5 w-50' onClick={()=> consultarAPI()}>Obtener frase</Button>
-    {mostrarComponente} 
+      <Button
+        variant="warning"
+        className="my-5 w-50"
+        onClick={() => consultarAPI()}
+      >
+        Obtener frase
+      </Button>
+      {/* { carga ? <Frase personaje={personaje}></Frase> : <Spinner />} */}
+      {mostrarComponente}
     </section>
   );
 }
